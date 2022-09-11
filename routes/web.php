a8b9771881caf3625c72f260cc7c76c13dfd9078
Rio Adiwijaya <99385779+rioadi14@users.noreply.github.com>
2022-09-12 02:26:38 +0700

    CRUD User

--- routes/web.php
+++ routes/web.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\FrontendController;
 use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\ProductGalleryController;
 use App\Http\Controllers\TransactionController;
+use App\Http\Controllers\UserController;
 
 /*
 |--------------------------------------------------------------------------
@@ -33,6 +34,9 @@ Route::middleware([ 'auth:sanctum', 'verified'])->name('dashboard.')->prefix('da
         ]);
         Route::resource('transaction', TransactionController::class)->only([
             'index', 'show', 'edit', 'update'
+        ]);
+        Route::resource('user', UserController::class)->only([
+            'index', 'edit', 'update', 'destroy'
         ]);
     });
 });
