681763d71d40484c704488db180043be2a2d8ef2
Rio Adiwijaya <99385779+rioadi14@users.noreply.github.com>
2022-09-16 22:47:48 +0700

    v3 d

--- routes/web.php
+++ routes/web.php
@@ -1,13 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\UserController;
-use App\Http\Controllers\ProductController;
 use App\Http\Controllers\FrontendController;
 use App\Http\Controllers\DashboardController;
-use App\Http\Controllers\TransactionController;
-use App\Http\Controllers\MyTransactionController;
-use App\Http\Controllers\ProductGalleryController;
 
 /*
 |--------------------------------------------------------------------------
@@ -23,7 +18,7 @@ use App\Http\Controllers\ProductGalleryController;
 Route::get('/', [FrontendController::class, 'index'])->name('index');
 Route::get('/details/{slug}', [FrontendController::class, 'details'])->name('details');
 
-Route::middleware([ 'auth:sanctum', 'verified'])->group(function (){
+Route::middleware(['auth:sanctum', 'verified'])->group(function (){
     Route::get('/cart', [FrontendController::class, 'cart'])->name('cart');
     Route::post('/cart/{id}', [FrontendController::class, 'cartAdd'])->name('cart-add');
     Route::delete('/cart/{id}', [FrontendController::class, 'cartDelete'])->name('cart-delete');
@@ -31,7 +26,7 @@ Route::middleware([ 'auth:sanctum', 'verified'])->group(function (){
     Route::get('/checkout/success', [FrontendController::class, 'success'])->name('checkout-success');
 });
 
-Route::middleware([ 'auth:sanctum', 'verified'])->name('dashboard.')->prefix('dashboard')->group(function (){
+Route::middleware(['auth:sanctum', 'verified'])->name('dashboard.')->prefix('dashboard')->group(function (){
     Route::get('/', [DashboardController::class, 'index'])->name('index');
     Route::resource('my-transaction', MyTransactionController::class)->only([
         'index', 'show'
